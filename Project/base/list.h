ddda8154e0698db4e817e0603d73630f469f7951
RodionovDenis <denisrodionov18@gmail.com>
2019-03-26 03:09:46 +0300

    hey

--- Project/base/list.h
+++ Project/base/list.h
@@ -1,4 +1,143 @@
 #ifndef __LIST_H__
-#define __LIST_H__
+#define __LIST_H__ 
+
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+struct Node //структура звена
+{
+	T data; //данные
+	Node<T> * pNext; //указатель на следующее звено
+	Node() : data(), pNext(nullptr) {} //коструктор по умолчанию
+};
+
+template <class T>
+class List
+{
+	Node<T> * pFirst;
+public:
+	List() : pFirst(nullptr) {} //конструктор
+	~List() //деструктор
+	{
+		Node<T> * p = pFirst;
+		while (p != nullptr)
+		{
+			pFirst = p->pNext;
+			delete p;
+			p = pFirst;
+		}
+	} //деструктор
+	List(const List<T> & list) //конструктор копирования
+	{
+		if (list.pFirst != nullptr)
+			pFirst = nullptr;
+		else
+		{
+			pFirst = new Node<T>;
+			Node<T> p1 = pFirst, p2 = list.pFirst;
+			while (p2->pNext != nullptr)
+			{
+				p1->data = p2->data;
+				p1 = p1->pNext;
+				p1 = new Node<T>;
+				p2 = p2->pNext;
+			}
+			p1->data = p2->data;
+			p1->pNext = nullptr;
+		}
+	}
+	List<T> & operator=(const List<T> & list) //оператор присваивания
+	{
+		if (list.pFirst != nullptr)
+		{
+			pFirst = nullptr;
+			return this;
+		}
+		if (pFirst != nullptr)
+		{
+			Link<T> * p = pFirst;
+			while (p != nullptr)
+			{
+				pFirst = p->pNext;
+				delete p;
+				p = pFirst;
+			}
+		}
+		pFirst = new Node<T>;
+		Node<T> p1 = pFirst, p2 = list.pFirst;
+		while (p2->pNext != nullptr)
+		{
+			p1->data = p2->data;
+			p1 = p1->pNext;
+			p1 = new Node<T>;
+			p2 = p2->pNext;
+		}
+		p1->data = p2->data;
+		return this;
+	}
+	int Size() //получить размер
+	{
+		int size = 0;
+		Node<T> * ptr = pFirst;
+		while (ptr != nullptr)
+		{
+			size++;
+			ptr = ptr->pNext;
+		}
+		return size;
+	}
+	T operator[](int i) //перегрузка скобок
+	{
+		if (i >= size)
+			throw "data is not correct";
+		int j = 0;
+		Node<T> * ptr = pFirst;
+		while (i != j)
+			ptr = ptr->pNext;
+		return ptr->data;
+	}
+	void Insert(T _data) //вставить
+	{
+		if (pFirst == nullptr)
+		{
+			pFirst = new Node<T>;
+			pFirst->data = _data;
+			return;
+		}
+		Node<T> * ptr = pFirst, * p = pFirst;
+		Node<T> * tmp = new Node<T>;
+		tmp->data = _data;
+		if (_data >= pFirst->data)
+		{
+			tmp->pNext = pFirst;
+			pFirst = tmp;
+			return;
+		}
+		while (ptr != nullptr)
+		{
+			if (_data >= ptr->data)
+			{
+				p->pNext = tmp;
+				tmp->pNext = ptr;
+				return;
+			}
+			p = ptr;
+			ptr = ptr->pNext;
+		}
+		p->pNext = tmp;
+	}
+	void Print()
+	{
+		Node<T> *ptr = pFirst;
+		while (ptr != nullptr)
+		{
+			cout << ptr->data << " ";
+			ptr = ptr->pNext;
+		}
+	}
+
+};
 
 #endif
--- Project/base/polynomials.h
+++ Project/base/polynomials.h
@@ -1,5 +1,106 @@
 #ifndef __POLYNOMIALS_H__
-#define __POLYNOMIALS_H__
+#define __POLYNOMIALS_H__ 
 
+#include "list.h"
+#include <string>
 
+struct Monom
+{
+	double coeff; //коэффициент
+	int deg; //свернутая степень
+	Monom(double _coeff, int _deg): coeff(_coeff), deg(_deg) {} // конструктор 
+	bool operator>= (const Monom & _monom) //перегрузка сравнения для монома
+	{
+		if (deg >= _monom.deg)
+			return true;
+		return false;
+	}
+};
+
+class Polynom
+{
+	List<Monom> monoms; //упорядоченный по степеням список мономов
+	string form; //строка - представление многочлена в алгебраическом виде 
+	string name; //имя полинома
+public:
+	Polynom(string _form, string _name) :monoms(), form(_form), name(_name) //конструктор1
+	{
+		string str;
+		for (int i = 0; i < form.size(); i++)
+		{
+			if (IsOperation(form[i]))
+			{
+				Monom mon(ReturnCoeff(str), ReturnDeg(str));
+				monoms.Insert(mon);
+				str.clear();
+				continue;
+			}
+			str += form[i];
+		}
+	}
+	Polynom(List<Monom> _monoms): monoms(_monoms), name("-") {} // конструктор2
+	{
+		string degs;
+		for (int i = 0; i < monoms.Size(); i++)
+		{
+			form += dtos(monoms[i].coeff);
+			degs = itos(monoms[i].deg);
+			form += 'x' += degs[0] += 'y' += degs[1] += 'z' += degs[2];
+		}
+	}
+	/*Polynom operator+(const Polynom & polynom)
+	{
+
+	}*/
+};
+
+bool IsVariable(char character) // переменная или нет?
+{
+	if (character == 'x' || character == 'y' || character == 'z')
+		return true;
+	else return false;
+}
+bool IsOperation(char ch) //операция?
+{
+	if (ch == '+' || ch == '-')
+		return true;
+	return false;
+}
+bool IsNumber(char character) // проверка на степень
+{
+	for (char i = '0'; i <= '9'; i++)
+		if (character == i)
+			return true;
+	return false;
+}
+bool IsValidCharacter(char ch) //проверка символа на допустимое значение
+{
+	if (IsVariable(ch) || IsOperation(ch) || IsNumber(ch))
+		return true;
+	return false;
+}
+bool IsPolynomCorrect(string polynom) //проверка строки с полиномом на корректность
+{
+	for (int i = 0; i < polynom.size(); i++)
+		if (!IsValidCharacter(polynom[i]))
+			return false;
+	return true;
+}
+double ReturnCoeff(string monom) //вернуть коэффициент из монома
+{
+	string flag;
+	for (int i = 0; i < monom.size(); i++)
+		if (monom[i] != 'x')
+			flag += monom[i];
+		else break;
+	return stod(flag);
+}
+int ReturnDeg(string monom) //вернуть свернутую степень у монома
+{
+	string flag;
+	for (int i = 0; i < monom.size(); i++)
+		if (monom[i] == 'x' || monom[i] == 'y' || monom[i] == 'z')
+			flag += monom[i + 1];
+	return stoi(flag);
+}
 #endif
